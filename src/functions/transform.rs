a3a016bf5c394cf60f304d9cde39d3fe17f2aeec
BPAmsler <manof2words@yahoo.com>
2024-10-29 18:54:26 -0700

    fixed lookat

--- src/functions/transform.rs
+++ src/functions/transform.rs
@@ -8,15 +8,20 @@ pub fn lookAt(eye: Vec3, center: Vec3, up: Vec3) -> Mat4 {
     let forward = normalize(eye - center);
     let right = normalize(cross(up, forward));
 
-    let tx = dot(eye, right);
-    let ty = dot(eye, up);
-    let tz = dot(eye, forward);
+    let [rx, ry, rz] = right.0.data.0[0];
+    let [fx, fy, fz] = forward.0.data.0[0];
+    let [ux, uy, uz] = up.0.data.0[0];
+
+    let tx = -dot(eye, right);
+    let ty = -dot(eye, up);
+    let tz = -dot(eye, forward);
 
     Mat4::_new(
-        right.0[0], right.0[1], right.0[2], tx,
-        up.0[0], up.0[1], up.0[2], ty,
-        forward.0[0], forward.0[1], forward.0[2], tz,
-        0.0, 0.0, 0.0, 1.0)
+        rx, ux, fx, 0.0,
+        ry, uy, fy, 0.0,
+        rz, uz, fz, 0.0,
+        tx, ty, tz, 1.0
+    )
 }
 
 // pub fn rotate(m: &Mat4, angle: f32, axis: &Vec3) -> Mat4 {
