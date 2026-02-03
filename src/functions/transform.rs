3a298edf644659f02ce03c1681c39167bd02c21d
Brian Amsler <manof2words@yahoo.com>
2026-02-03 10:08:56 -0800

    fix various functions for left-handed coordinates

--- src/functions/transform.rs
+++ src/functions/transform.rs
@@ -5,17 +5,21 @@ use crate::{matrices::Mat4, vectors::Vec3};
 use super::geometric::{cross, dot, normalize};
 
 pub fn lookAt(eye: Vec3, center: Vec3, up: Vec3) -> Mat4 {
-    let forward = normalize(eye - center);
-    let right = normalize(cross(up, forward));
-    let up = cross(forward, right);
+    let back = normalize(eye - center);
+    let right = normalize(cross(back, up));
+    let up = cross(right, back);
+
+    println!("back: {:?}", back);
+    println!("right: {:?}", right);
+    println!("up: {:?}", up);
 
     let [rx, ry, rz] = right.0.data.0[0];
-    let [fx, fy, fz] = forward.0.data.0[0];
+    let [fx, fy, fz] = back.0.data.0[0];
     let [ux, uy, uz] = up.0.data.0[0];
 
     let tx = dot(eye, right);
     let ty = dot(eye, up);
-    let tz = dot(eye, forward);
+    let tz = dot(eye, back * -1);
 
     Mat4::_new(
         rx, ry, rz, -tx,
